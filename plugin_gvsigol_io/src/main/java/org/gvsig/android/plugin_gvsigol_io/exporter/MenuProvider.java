825f83ab2215b513037b82d8b749f4c182af18b9
dispiste <cesar.izq@gmail.com>
2017-02-10 18:34:03 +0100

    Migrate spatialite exporter

--- plugin_gvsigol_io/src/main/java/org/gvsig/android/plugin_gvsigol_io/exporter/MenuProvider.java
+++ plugin_gvsigol_io/src/main/java/org/gvsig/android/plugin_gvsigol_io/exporter/MenuProvider.java
@@ -1,4 +1,4 @@
-package org.gvsig.android.plugin_gvsigol_io.importer;
+package org.gvsig.android.plugin_gvsigol_io.exporter;
 
 import android.content.Context;
 import android.content.Intent;
@@ -6,16 +6,23 @@ import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 
-
 import org.gvsig.android.plugin_gvsigol_io.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import eu.geopaparazzi.core.utilities.Constants;
+import eu.geopaparazzi.library.core.maps.SpatialiteMap;
 import eu.geopaparazzi.library.network.NetworkUtilities;
 import eu.geopaparazzi.library.plugin.PluginService;
 import eu.geopaparazzi.library.plugin.types.MenuEntry;
 import eu.geopaparazzi.library.plugin.types.MenuEntryList;
 import eu.geopaparazzi.library.util.GPDialogs;
 import eu.geopaparazzi.library.util.IActivitySupporter;
+import eu.geopaparazzi.library.util.LibraryConstants;
+import eu.geopaparazzi.spatialite.database.spatial.SpatialiteSourcesManager;
+
+import static eu.geopaparazzi.library.util.LibraryConstants.DATABASE_ID;
 
 
 /**
@@ -44,23 +51,37 @@ public class MenuProvider extends PluginService {
         }
 
         @Override
-        protected boolean processOnClick(IActivitySupporter starter) {
+        public void onClick(IActivitySupporter starter) {
             Context context = starter.getContext();
+
             if (!NetworkUtilities.isNetworkAvailable(context)) {
                 GPDialogs.infoDialog(context, context.getString(R.string.available_only_with_network), null);
-                return false;
+                return;
             }
 
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
             final String user = preferences.getString(Constants.PREF_KEY_USER, "geopaparazziuser"); //$NON-NLS-1$
             final String pwd = preferences.getString(Constants.PREF_KEY_PWD, "geopaparazzipwd"); //$NON-NLS-1$
-            final String url = preferences.getString(Constants.PREF_KEY_SERVER, ""); //$NON-NLS-1$
-
-            if (url.length() == 0 || user.length() == 0 || pwd.length() == 0) {
+            final String serverUrl = preferences.getString(Constants.PREF_KEY_SERVER, ""); //$NON-NLS-1$
+            if (serverUrl.length() == 0) {
                 GPDialogs.infoDialog(context, getString(R.string.error_set_cloud_settings_gvsigol), null);
-                return false;
+                return;
             }
-            return true;
+
+            Intent webExportIntent = new Intent(context, SpatialiteExporterActivity.class);
+            webExportIntent.putExtra(LibraryConstants.PREFS_KEY_URL, serverUrl);
+            webExportIntent.putExtra(LibraryConstants.PREFS_KEY_USER, user);
+            webExportIntent.putExtra(LibraryConstants.PREFS_KEY_PWD, pwd);
+            List<SpatialiteMap> spatialiteMaps = SpatialiteSourcesManager.INSTANCE.getSpatialiteMaps();
+            List<String> databases = new ArrayList<String>();
+            for (int i = 0; i < spatialiteMaps.size(); i++) {
+                String dbPath = spatialiteMaps.get(i).databasePath;
+                if (!databases.contains(dbPath)) {
+                    databases.add(dbPath);
+                }
+            }
+            webExportIntent.putExtra(DATABASE_ID, databases.toArray(new String[0]));
+            startActivity(webExportIntent);
         }
     }
 
